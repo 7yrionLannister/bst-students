12e919ea6a7fbe13cfe19196a56ef2b00b85605c
Daniel Alejandro Fernandez Robles <1007146215@academico.icesi.edu.co>
2019-05-15 12:15:01 -0500

    More scenarios

--- test/model/CourseTest.java
+++ test/model/CourseTest.java
@@ -3,10 +3,14 @@ package model;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class CourseTest {
 	private Course course;
+	private ArrayList<Student> trail;
 	
 	private void setupScenary1() {
 		course = new Course();
@@ -31,6 +35,32 @@ class CourseTest {
 		course.addStudent(20, "Viserys Targaryen");
 	}
 	
+	private void setupScenary3preorder() {
+		setupScenary1();
+		trail = new ArrayList<Student>();
+		trail.add(new Student(5678, "A"));
+		trail.add(new Student(5542, "Cinco"));
+		trail.add(new Student(5308, "Don juan"));
+		trail.add(new Student(5398, "PIB"));
+		trail.add(new Student(5582, "A"));
+		trail.add(new Student(6789, "b"));
+		trail.add(new Student(6788, "v"));
+		trail.add(new Student(5912, "c"));
+		trail.add(new Student(7890, "z"));
+	}
+	
+	private void setupStage1() {
+		setupScenary3preorder();
+		trail.get(0).setLeft(trail.get(1));
+		trail.get(1).setLeft(trail.get(2));
+		trail.get(2).setRight(trail.get(3));
+		trail.get(1).setRight(trail.get(4));
+		trail.get(0).setRight(trail.get(5));
+		trail.get(5).setLeft(trail.get(6));
+		trail.get(6).setLeft(trail.get(7));
+		trail.get(5).setRight(trail.get(8));
+	}
+	
 	@Test
 	public void searchTest() {
 		setupScenary1();
@@ -44,6 +74,24 @@ class CourseTest {
 		
 		found = course.searchStudent(15);
 		assertNull("The student is not in the BST so the value must be null", found);
+	}
+	
+	@Test
+	public void inorderTest() {
+		setupScenary2();
+		ArrayList<Student> inorder = (ArrayList)course.inorder();
+		for(int i = 1; i < inorder.size(); i++) {
+			assertTrue("The method does not return the tree in a list in ascending order", inorder.get(i-1).compareTo(inorder.get(i)) <= 0);
+		}
+	}
+	
+	@Test
+	public void preorderTest() {
+		setupScenary3preorder();
+		ArrayList<Student> preorder = (ArrayList<Student>)course.preorder();
+		for(int i = 0; i < preorder.size(); i++) {
+			assertTrue("The list is not in preorder", preorder.get(i).compareTo(trail.get(i)) == 0);
+		}
 	}
 	
 	@Test 
